45f864e36219b2d9040398c507aa71da64d07e53
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2024-12-08 11:11:28 -0800

    FILE SOM

--- FILE/SOM/INFORM.sh
+++ FILE/SOM/INFORM.sh
@@ -39,5 +39,5 @@ CONFIG_PATH="${BASE_FOLDER}SOM/INFORM/INFORM.yaml"
 
 # Run applications
 python -u "${BASE_PATH}FILE/SOM/INFORM.py" --number=$NUMBER --folder=$BASE_FOLDER &
-srun -u -N 1 -n 1 --cpus-per-task=$SLURM_CPUS_PER_TASK python -m ceci rail.estimation.algos.somoclu_som.SOMocluInformer --mpi --memmon --name=$NAME --input=$INPUT_PATH --model=$MODEL_PATH --config=$CONFIG_PATH & 
+srun -u -N 1 -n 1 --cpus-per-task=$SLURM_CPUS_PER_TASK python -m ceci rail.estimation.algos.somoclu_som.SOMocluInformer --mpi --name=$NAME --input=$INPUT_PATH --model=$MODEL_PATH --config=$CONFIG_PATH & 
 wait
