e36d6cda2aa5f2da9cb3e43fcd53919e7747134d
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2024-12-14 11:48:45 -0800

    FILE FZB

--- FILE/FZB/SUMMARIZE_SOURCE.sh
+++ FILE/FZB/SUMMARIZE_SOURCE.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH -A m1727
-#SBATCH --nodes=1
+#SBATCH --nodes=5
 #SBATCH -q regular
-#SBATCH --ntasks=1
 #SBATCH --time=48:00:00
 #SBATCH --mail-type=END
 #SBATCH --constraint=cpu
 #SBATCH -o LOG/%x_%j.out
-#SBATCH --cpus-per-task=256
+#SBATCH --cpus-per-task=4
+#SBATCH --ntasks-per-node=64
 #SBATCH -J FZB_SUMMARIZE_SOURCE
 #SBATCH --mail-user=YunHao.Zhang@ed.ac.uk
 
@@ -27,10 +27,28 @@ export OMP_PLACES=threads
 export OMP_PROC_Bind=spread
 
 # Initialize the process
-COUNT=16
+COUNT=5
 NUMBER=400
 BASE_PATH="/pscratch/sd/y/yhzhang/ZCloud/"
 BASE_FOLDER="/global/cfs/cdirs/lsst/groups/PZ/users/yhzhang/ZCloud/"
 
-# Run applications
-srun -n 1 --cpu-bind=none python -u "${BASE_PATH}FILE/FZB/SUMMARIZE_SOURCE.py" --path=$BASE_PATH --COUNT=$COUNT --NUMBER=$NUMBER
+for BIN in $(seq 1 $COUNT); do 
+    for INDEX in $(seq 1 $NUMBER); do
+        # Set path of input variables
+        NAME="SUMMARIZE_SOURCE${INDEX}"
+        INPUT_PATH="${BASE_FOLDER}/FZB/SOURCE/SOURCE${INDEX}/SAMPLE${BIN}.hdf5"
+        CONFIG_PATH="${BASE_FOLDER}/FZB/SOURCE/SOURCE${INDEX}/SUMMARIZE${BIN}.yaml"
+        # Set path of output variables
+        SINGLE_PATH="${BASE_FOLDER}/FZB/SOURCE/SOURCE${INDEX}/SINGLE${BIN}.hdf5"
+        OUTPUT_PATH="${BASE_FOLDER}/FZB/SOURCE/SOURCE${INDEX}/SUMMARIZE${BIN}.hdf5"
+        # Run applications
+        python -u "${BASE_PATH}/FILE/FZB/SUMMARIZE_SOURCE.py" --bin=$BIN --index=$INDEX --folder=$BASE_FOLDER &
+        srun -u -N 1 -n 1 --cpus-per-task=$SLURM_CPUS_PER_TASK python -m ceci rail.estimation.algos.naive_stack.NaiveStackSummarizer --mpi --name=$NAME --input=$INPUT_PATH --config=$CONFIG_PATH --single_NZ=$SINGLE_PATH --output=$OUTPUT_PATH &
+        # Control parallel execution
+        if (( $INDEX % $SLURM_NTASKS_PER_NODE == 0 )); then
+            wait 
+        fi
+    done
+    wait
+done
+wait
