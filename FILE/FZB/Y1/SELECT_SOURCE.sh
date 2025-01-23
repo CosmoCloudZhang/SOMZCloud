4cc4ae0ad68fb5c5493df86fc5c1128654a7915b
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2025-01-22 23:55:32 -0800

    FILE/FZB/

--- FILE/FZB/Y1/SELECT_SOURCE.sh
+++ FILE/FZB/Y1/SELECT_SOURCE.sh
@@ -2,11 +2,11 @@
 #SBATCH -A m1727
 #SBATCH --nodes=4
 #SBATCH -q regular
+#SBATCH -J FZB_SELECT
 #SBATCH --time=24:00:00
 #SBATCH --mail-type=END
 #SBATCH --constraint=cpu
 #SBATCH -o LOG/%x_%j.out
-#SBATCH -J FZB_Y1_FIGURE
 #SBATCH --cpus-per-task=64
 #SBATCH --ntasks-per-node=4
 #SBATCH --mail-user=YunHao.Zhang@ed.ac.uk
@@ -29,17 +29,16 @@ export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
 
 # Initialize the process
-TAG="Y1"
-NUMBER=500
+NUMBER=400
 BASE_PATH="/pscratch/sd/y/yhzhang/ZCloud/"
 BASE_FOLDER="/global/cfs/cdirs/lsst/groups/PZ/users/CosmoCloud/ZCloud/"
 
 # Run applications
 for INDEX in $(seq 1 $NUMBER); do
-    srun -u -N 1 -n 1 -c $SLURM_CPUS_PER_TASK python -u "${BASE_PATH}FILE/FZB/${TAG}/FIGURE.py" --index=$INDEX --folder=$BASE_FOLDER & 
+    srun -u -N 1 -n 1 --cpu-bind=none python -u "${BASE_PATH}/FILE/FZB/SELECT.py" --index=$INDEX --folder=$BASE_FOLDER &
     # Control parallel execution
     if (( $INDEX % $SLURM_NTASKS == 0 )); then
-        wait
+        wait 
     fi
 done
 wait
--- FILE/FZB/Y10/SELECT_SOURCE.sh
+++ FILE/FZB/Y10/SELECT_SOURCE.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH -A m1727
-#SBATCH --nodes=1
+#SBATCH --nodes=4
 #SBATCH -q regular
-#SBATCH --time=04:00:00
+#SBATCH -J FZB_SELECT
+#SBATCH --time=24:00:00
 #SBATCH --mail-type=END
 #SBATCH --constraint=cpu
 #SBATCH -o LOG/%x_%j.out
-#SBATCH --cpus-per-task=256
-#SBATCH --ntasks-per-node=1
-#SBATCH -J FZB_ENSEMBLE_FIGURE
+#SBATCH --cpus-per-task=64
+#SBATCH --ntasks-per-node=4
 #SBATCH --mail-user=YunHao.Zhang@ed.ac.uk
 
 # Load modules
@@ -22,15 +22,22 @@ source $HOME/.bashrc
 conda activate $RAILENV
 
 # Set environment
-export NUMEXPR_MAX_THREADS=$SLURM_CPUS_PER_TASK
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export HDF5_USE_FILE_LOCKING=FALSE
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
 
 # Initialize the process
+NUMBER=400
 BASE_PATH="/pscratch/sd/y/yhzhang/ZCloud/"
 BASE_FOLDER="/global/cfs/cdirs/lsst/groups/PZ/users/CosmoCloud/ZCloud/"
 
 # Run applications
-python -u "${BASE_PATH}FILE/FZB/ENSEMBLE_FIGURE.py" --folder=$BASE_FOLDER
+for INDEX in $(seq 1 $NUMBER); do
+    srun -u -N 1 -n 1 --cpu-bind=none python -u "${BASE_PATH}/FILE/FZB/SELECT.py" --index=$INDEX --folder=$BASE_FOLDER &
+    # Control parallel execution
+    if (( $INDEX % $SLURM_NTASKS == 0 )); then
+        wait 
+    fi
+done
+wait
