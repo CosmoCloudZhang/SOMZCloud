0e544b09edd2ae89a6c8235e809a39ce7842c467
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2025-02-12 14:02:06 -0800

    FILE/SOM/

---
+++ FILE/SOM/Y1/HISTOGRAM_SOURCE.py
@@ -0,0 +1,144 @@
+import os
+import time
+import h5py
+import numpy
+import scipy
+import argparse
+
+
+def main(tag, index, folder):
+    '''
+    Histogram of the spectroscopic redshifts of the source samples
+    
+    Arguments:
+        tag (str): The tag of the configuration
+        index (int): The index of all the datasets
+        folder (str): The base folder of all the datasets
+    
+    Returns:
+        duration (float): The duration of the process.
+    '''
+    # Data store
+    start = time.time()
+    print('Index: {}'.format(index))
+    
+    # Path
+    som_folder = os.path.join(folder, 'SOM/')
+    fzb_folder = os.path.join(folder, 'FZB/')
+    dataset_folder = os.path.join(folder, 'DATASET/')
+    
+    os.makedirs(os.path.join(som_folder, '{}/SOURCE/'.format(tag)), exist_ok=True)
+    os.makedirs(os.path.join(som_folder, '{}/SOURCE/SOURCE{}'.format(tag, index)), exist_ok=True)
+    
+    # Redshift
+    z1 = 0.0
+    z2 = 3.0
+    grid_size = 300
+    z_delta = (z2 - z1) / grid_size
+    z_grid = numpy.linspace(z1, z2, grid_size + 1)
+    z_bin = numpy.linspace(z1 - z_delta / 2, z2 + z_delta / 2, z_grid.size + 1)
+    
+    # Application
+    with h5py.File(os.path.join(dataset_folder, '{}/APPLICATION/DATA{}.hdf5'.format(tag, index)), 'r') as file:
+        application_cell_id = file['meta']['cell_id'][...]
+        application_sigma = file['photometry']['sigma'][...]
+        application_redshift_true = file['photometry']['redshift_true'][...]
+    
+    # Select
+    with h5py.File(os.path.join(fzb_folder, '{}/SELECT/DATA{}.hdf5'.format(tag, index)), 'r') as file:
+        bin_source = file['bin_source'][...]
+    
+    with h5py.File(os.path.join(fzb_folder, '{}/SOURCE/SOURCE{}/SELECT.hdf5'.format(tag, index)), 'r') as file:
+        select_source = file['select'][...]
+    
+    # Combination
+    with h5py.File(os.path.join(dataset_folder, '{}/COMBINATION/DATA{}.hdf5'.format(tag, index)), 'r') as file:
+        combination_cell_id = file['meta']['cell_id'][...]
+    
+    # Reference
+    with h5py.File(os.path.join(fzb_folder, '{}/REFERENCE/DATA{}.hdf5'.format(tag, index)), 'r') as file:
+        bin_source = file['bin_source'][...]
+    
+    with h5py.File(os.path.join(fzb_folder, '{}/SOURCE/SOURCE{}/REFERENCE.hdf5'.format(tag, index)), 'r') as file:
+        reference_source = file['reference'][...]
+    
+    # Size
+    sample_size = 100
+    bin_source_size = len(bin_source) - 1
+    
+    # Lens
+    single_source = numpy.zeros((bin_source_size, grid_size + 1))
+    sample_source = numpy.zeros((bin_source_size, sample_size, grid_size + 1))
+    
+    # Loop
+    for m in range(bin_source_size):
+        # Select
+        select = select_source[m, :]
+        select_size = numpy.sum(select)
+        
+        # Reference
+        reference = reference_source[m, :]
+        reference_size = numpy.sum(reference)
+        
+        # Application
+        application_sigma_select = application_sigma[select]
+        application_cell_id_select = application_cell_id[select]
+        application_redshift_true_select = application_redshift_true[select]
+        
+        # Combination
+        combination_cell_id_reference = combination_cell_id[reference]
+        
+        # Weight
+        weight = numpy.array(numpy.isin(application_cell_id_select, combination_cell_id_reference), dtype=numpy.float32) / numpy.square(application_sigma_select)
+        
+        # Single
+        histogram = numpy.histogram(application_redshift_true_select, bins=z_bin, range=(z1, z2), weights=weight, density=True)[0]
+        single_source[m, :] = histogram / scipy.integrate.trapezoid(x=z_grid, y=histogram, axis=0)
+        
+        # Bootstrap
+        for n in range(sample_size):
+            # Application
+            application_indices = numpy.random.choice(numpy.arange(select_size), select_size, replace=True)
+            application_redshift_true_sample = application_redshift_true_select[application_indices]
+            application_cell_id_sample = application_cell_id_select[application_indices]
+            application_sigma_sample = application_sigma_select[application_indices]
+            
+            # Combination
+            combination_indices = numpy.random.choice(numpy.arange(reference_size), reference_size, replace=True)
+            combination_cell_id_sample = combination_cell_id_reference[combination_indices]
+            
+            # Weight
+            weight_sample = numpy.array(numpy.isin(application_cell_id_sample, combination_cell_id_sample), dtype=numpy.float32) / numpy.square(application_sigma_sample)
+            
+            # Sample
+            histogram = numpy.histogram(application_redshift_true_sample, bins=z_bin, range=(z1, z2), weights=weight_sample, density=True)[0]
+            sample_source[m, n, :] = histogram / scipy.integrate.trapezoid(x=z_grid, y=histogram, axis=0)
+    
+    # Save
+    with h5py.File(os.path.join(som_folder, '{}/SOURCE/SOURCE{}/HISTOGRAM.hdf5'.format(tag, index)), 'w') as file:
+        file.create_dataset('single', data=single_source, dtype=numpy.float32)
+        file.create_dataset('sample', data=sample_source, dtype=numpy.float32)
+    
+    # Duration
+    end = time.time()
+    duration = (end - start) / 60
+    
+    # Return
+    print('Time: {:.2f} minutes'.format(duration))
+    return duration
+
+
+if __name__ == '__main__':
+    # Input
+    PARSE = argparse.ArgumentParser(description='FZB Histogram')
+    PARSE.add_argument('--tag', type=str, required=True, help='The tag of the configuration')
+    PARSE.add_argument('--index', type=int, required=True, help='The index of all the datasets')
+    PARSE.add_argument('--folder', type=str, required=True, help='The base folder of all the datasets')
+    
+    # Parse
+    TAG = PARSE.parse_args().tag
+    INDEX = PARSE.parse_args().index
+    FOLDER = PARSE.parse_args().folder
+    
+    # Output
+    OUTPUT = main(TAG, INDEX, FOLDER)
---
+++ FILE/SOM/Y1/SUMMARIZE_LENS.py
@@ -0,0 +1,134 @@
+import os
+import time
+import h5py
+import numpy
+import scipy
+import argparse
+
+def main(tag, index, folder):
+    '''
+    Histogram of the spectroscopic redshifts of the lens samples
+    
+    Arguments:
+        tag (str): The tag of the configuration
+        index (int): The index of all the datasets
+        folder (str): The base folder of all the datasets
+    
+    Returns:
+        duration (float): The duration of the process.
+    '''
+    # Data store
+    start = time.time()
+    print('Index: {}'.format(index))
+    
+    # Path
+    som_folder = os.path.join(folder, 'SOM/')
+    fzb_folder = os.path.join(folder, 'FZB/')
+    dataset_folder = os.path.join(folder, 'DATASET/')
+    
+    os.makedirs(os.path.join(som_folder, '{}/LENS/'.format(tag)), exist_ok=True)
+    os.makedirs(os.path.join(som_folder, '{}/LENS/LENS{}'.format(tag, index)), exist_ok=True)
+    
+    # Redshift
+    z1 = 0.0
+    z2 = 3.0
+    grid_size = 300
+    z_delta = (z2 - z1) / grid_size
+    z_grid = numpy.linspace(z1, z2, grid_size + 1)
+    z_bin = numpy.linspace(z1 - z_delta / 2, z2 + z_delta / 2, z_grid.size + 1)
+    
+    # Application
+    with h5py.File(os.path.join(dataset_folder, '{}/APPLICATION/DATA{}.hdf5'.format(tag, index)), 'r') as file:
+        application_cell_id = file['meta']['cell_id'][...]
+        application_cell_size = file['meta']['cell_size'][...]
+    
+    # Combination
+    with h5py.File(os.path.join(dataset_folder, '{}/COMBINATION/DATA{}.hdf5'.format(tag, index)), 'r') as file:
+        combination_cell_id = file['meta']['cell_id'][...]
+        combination_cell_size = file['meta']['cell_size'][...]
+        combination_cell_count = file['meta']['cell_count'][...]
+        combination_redshift = file['photometry']['redshift'][...]
+    combination_size = len(combination_cell_id)
+    
+    # Select
+    with h5py.File(os.path.join(fzb_folder, '{}/SELECT/DATA{}.hdf5'.format(tag, index)), 'r') as file:
+        bin_lens = file['bin_lens'][...]
+    
+    with h5py.File(os.path.join(fzb_folder, '{}/LENS/LENS{}/SELECT.hdf5'.format(tag, index)), 'r') as file:
+        select_lens = file['select'][...]
+    select_lens = select_lens & (numpy.isin(application_cell_id, combination_cell_id))[numpy.newaxis, :]
+    
+    # Size
+    sample_size = 100
+    bin_lens_size = len(bin_lens) - 1
+    
+    # Lens
+    single_lens = numpy.zeros((bin_lens_size, grid_size + 1))
+    sample_lens = numpy.zeros((bin_lens_size, sample_size, grid_size + 1))
+    
+    # Loop
+    for m in range(bin_lens_size):
+        # Select
+        select = select_lens[m, :] 
+        select_size = numpy.sum(select)
+        
+        # Application
+        application_cell_id_select = application_cell_id[select]
+        application_cell_count_select = numpy.bincount(application_cell_id_select, minlength=application_cell_size)
+        
+        # Combination
+        combination_cell_weight_select = numpy.divide(application_cell_count_select, combination_cell_count, out=numpy.zeros(combination_cell_size), where=combination_cell_count != 0)
+        combination_weight_select = combination_cell_weight_select[combination_cell_id]
+        
+        # Single
+        histogram = numpy.histogram(combination_redshift, bins=z_bin, weights=combination_weight_select)[0]
+        single_lens[m, :] = histogram / scipy.integrate.trapezoid(x=z_grid, y=histogram, axis=0)
+        
+        # Bootstrap
+        for n in range(sample_size):
+            # Application
+            application_indices = numpy.random.choice(numpy.arange(select_size), size=select_size, replace=True)
+            
+            application_cell_id_sample = application_cell_id_select[application_indices]
+            application_cell_count_sample = numpy.bincount(application_cell_id_sample, minlength=application_cell_size)
+            
+            # Combination
+            combination_indices = numpy.random.choice(numpy.arange(combination_size), size=combination_size, replace=True)
+            
+            combination_cell_id_sample = combination_cell_id[combination_indices]
+            combination_cell_count_sample = numpy.bincount(combination_cell_id_sample, minlength=combination_cell_size)
+            
+            combination_cell_weight_sample = numpy.divide(application_cell_count_sample, combination_cell_count_sample, out=numpy.zeros(combination_cell_size), where=combination_cell_count_sample != 0)
+            combination_weight_sample = combination_cell_weight_sample[combination_cell_id_sample]
+            
+            # Sample
+            histogram = numpy.histogram(combination_redshift[combination_indices], bins=z_bin, weights=combination_weight_sample)[0]
+            sample_lens[m, n, :] = histogram / scipy.integrate.trapezoid(x=z_grid, y=histogram, axis=0)
+    
+    # Save
+    with h5py.File(os.path.join(som_folder, '{}/LENS/LENS{}/SUMMARIZE.hdf5'.format(tag, index)), 'w') as file:
+        file.create_dataset('single', data=single_lens, dtype=numpy.float32)
+        file.create_dataset('sample', data=sample_lens, dtype=numpy.float32)
+    
+    # Return
+    end = time.time()
+    duration = (end - start) / 60
+    
+    print('Time: {:.2f} minutes'.format(duration))
+    return duration
+
+
+if __name__ == '__main__':
+    # Input
+    PARSE = argparse.ArgumentParser(description='FZB Summarize')
+    PARSE.add_argument('--tag', type=str, required=True, help='The tag of the configuration')
+    PARSE.add_argument('--index', type=int, required=True, help='The index of all the datasets')
+    PARSE.add_argument('--folder', type=str, required=True, help='The base folder of all the datasets')
+    
+    # Parse
+    TAG = PARSE.parse_args().tag
+    INDEX = PARSE.parse_args().index
+    FOLDER = PARSE.parse_args().folder
+    
+    # Output
+    OUTPUT = main(TAG, INDEX, FOLDER)
