e91aa9471fa116815a7faaeeb4f567f1039ba086
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2025-02-13 03:52:02 -0800

    FILE/ENSEMBLE/

---
+++ FILE/ENSEMBLE/Y10/HISTOGRAM_SOURCE.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH -A m1727
+#SBATCH --nodes=1
+#SBATCH -q regular
+#SBATCH --time=04:00:00
+#SBATCH --mail-type=END
+#SBATCH --constraint=cpu
+#SBATCH -o LOG/%x_%j.out
+#SBATCH --cpus-per-task=256
+#SBATCH --ntasks-per-node=1
+#SBATCH -J ENSEMBLE_Y10_HISTOGRAM_SOURCE
+#SBATCH --mail-user=YunHao.Zhang@ed.ac.uk
+
+# Load modules
+module load python
+module load PrgEnv-gnu
+module load cray-mpich/8.1.28
+module load cray-hdf5-parallel
+
+# Activate the conda environment
+source $HOME/.bashrc
+conda activate $RAILENV
+
+# Set environment
+export NUMEXPR_MAX_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export HDF5_USE_FILE_LOCKING=FALSE
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+
+# Initialize the process
+TAG="Y10"
+NUMBER=500
+BASE_PATH="/pscratch/sd/y/yhzhang/ZCloud/"
+BASE_FOLDER="/global/cfs/cdirs/lsst/groups/MCP/CosmoCloud/ZCloud/"
+
+# Run applications
+srun -u -N 1 -n 1 -c $SLURM_CPUS_PER_TASK python -u "${BASE_PATH}FILE/ENSEMBLE/${TAG}/HISTOGRAM_SOURCE.py" --tag=$TAG --number=$NUMBER --folder=$BASE_FOLDER 
