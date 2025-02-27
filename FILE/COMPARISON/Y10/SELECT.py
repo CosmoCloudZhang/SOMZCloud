26677a6bf13d24ffd43ceb989527bf6be4479e50
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2025-02-27 06:23:48 -0800

    FILE/COMPARISON/

--- FILE/COMPARISON/Y10/SELECT.py
+++ FILE/COMPARISON/Y10/SELECT.py
@@ -27,6 +27,12 @@ def main(tag, index, folder):
     comparison_folder = os.path.join(folder, 'COMPARISON/')
     os.makedirs(os.path.join(comparison_folder, '{}/SELECT/'.format(tag)), exist_ok=True)
     
+    os.makedirs(os.path.join(comparison_folder, '{}/LENS/'.format(tag)), exist_ok=True)
+    os.makedirs(os.path.join(comparison_folder, '{}/LENS/LENS{}/'.format(tag, index)), exist_ok=True)
+    
+    os.makedirs(os.path.join(comparison_folder, '{}/SOURCE/'.format(tag)), exist_ok=True)
+    os.makedirs(os.path.join(comparison_folder, '{}/SOURCE/SOURCE{}/'.format(tag, index)), exist_ok=True)
+    
     # Redshift
     z1_lens = 0.2
     z2_lens = 1.2
@@ -103,21 +109,83 @@ def main(tag, index, folder):
     
     # Save
     with h5py.File(os.path.join(comparison_folder, '{}/SELECT/DATA{}.hdf5'.format(tag, index)), 'w') as file:
-        file.create_dataset('z_phot', data=z_phot)
+        file.create_dataset('z_phot', data=z_phot, dtype=numpy.float32)
         
-        file.create_dataset('bin_lens', data=bin_lens)
-        file.create_dataset('select_lens', data=select_lens)
+        file.create_dataset('bin_lens', data=bin_lens, dtype=numpy.float32)
+        file.create_dataset('select_lens', data=select_lens, dtype=bool)
         
-        file.create_dataset('nmad_lens', data=nmad_lens)
-        file.create_dataset('fraction_lens', data=fraction_lens)
-        file.create_dataset('percentile_lens', data=percentile_lens)
+        file.create_dataset('nmad_lens', data=nmad_lens, dtype=numpy.float32)
+        file.create_dataset('fraction_lens', data=fraction_lens, dtype=numpy.float32)
+        file.create_dataset('percentile_lens', data=percentile_lens, dtype=numpy.float32)
         
-        file.create_dataset('bin_source', data=bin_source)
-        file.create_dataset('select_source', data=select_source)
+        file.create_dataset('bin_source', data=bin_source, dtype=numpy.float32)
+        file.create_dataset('select_source', data=select_source, dtype=bool)
         
-        file.create_dataset('nmad_source', data=nmad_source)
-        file.create_dataset('fraction_source', data=fraction_source)
-        file.create_dataset('percentile_source', data=percentile_source)
+        file.create_dataset('nmad_source', data=nmad_source, dtype=numpy.float32)
+        file.create_dataset('fraction_source', data=fraction_source, dtype=numpy.float32)
+        file.create_dataset('percentile_source', data=percentile_source, dtype=numpy.float32)
+    
+    # Lens bin
+    nmad_lens_bin = numpy.zeros(lens_size, dtype=numpy.float32)
+    fraction_lens_bin = numpy.zeros(lens_size, dtype=numpy.float32)
+    percentile_lens_bin = numpy.zeros(lens_size, dtype=numpy.float32)
+    select_lens_bin = numpy.ones((lens_size, application_size), dtype=bool)
+    
+    for m in range(len(bin_lens) - 1):
+        select_lens_bin[m, :] = select_lens & (bin_lens[m] <= z_phot) & (z_phot < bin_lens[m + 1])
+        
+        if numpy.sum(select_lens_bin[m, :]) > 0:
+            z_phot_lens_bin = z_phot[select_lens_bin[m, :]]
+            z_true_lens_bin = application_redshift_true[select_lens_bin[m, :]]
+            
+            sigma_lens_bin = numpy.abs(z_phot_lens_bin - z_true_lens_bin) / (1 + z_true_lens_bin)
+            nmad_lens_bin[m] = 1.4826 * numpy.median(numpy.abs(sigma_lens_bin - numpy.median(sigma_lens_bin)))
+            
+            fraction_lens_bin[m] = len(sigma_lens_bin[sigma_lens_bin > 0.15]) / len(sigma_lens_bin) * 100
+            percentile_lens_bin[m] = len(sigma_lens_bin[numpy.abs(z_phot_lens_bin - z_true_lens_bin) > 1.0]) / len(sigma_lens_bin) * 100
+        
+        else:
+            nmad_lens_bin[m] = numpy.nan
+            fraction_lens_bin[m] = numpy.nan
+            percentile_lens_bin[m] = numpy.nan
+    
+    with h5py.File(os.path.join(comparison_folder, '{}/LENS/LENS{}/SELECT.hdf5'.format(tag, index)), 'w') as file:    
+        file.create_dataset('select', data=select_lens_bin, dtype=bool)
+        
+        file.create_dataset('nmad', data=nmad_lens_bin, dtype=numpy.float32)
+        file.create_dataset('fraction', data=fraction_lens_bin, dtype=numpy.float32)
+        file.create_dataset('percentile', data=percentile_lens_bin, dtype=numpy.float32)
+    
+    # Source
+    nmad_source_bin = numpy.zeros(source_size, dtype=numpy.float32)
+    fraction_source_bin = numpy.zeros(source_size, dtype=numpy.float32)
+    percentile_source_bin = numpy.zeros(source_size, dtype=numpy.float32)
+    select_source_bin = numpy.ones((source_size, application_size), dtype=bool)
+    
+    for m in range(len(bin_source) - 1):
+        select_source_bin[m, :] = select_source & (bin_source[m] <= z_phot) & (z_phot < bin_source[m + 1])
+        
+        if numpy.sum(select_source_bin[m, :]) > 0 :
+            z_phot_source_bin = z_phot[select_source_bin[m, :]]
+            z_true_source_bin = application_redshift_true[select_source_bin[m, :]]
+            
+            sigma_source_bin = numpy.abs(z_phot_source_bin - z_true_source_bin) / (1 + z_true_source_bin)
+            nmad_source_bin[m] = 1.4826 * numpy.median(numpy.abs(sigma_source_bin - numpy.median(sigma_source_bin)))
+            
+            fraction_source_bin[m] = len(sigma_source_bin[sigma_source_bin > 0.15]) / len(sigma_source_bin) * 100
+            percentile_source_bin[m] = len(sigma_source_bin[numpy.abs(z_phot_source_bin - z_true_source_bin) > 1.0]) / len(sigma_source_bin) * 100
+        
+        else:
+            nmad_source_bin[m] = numpy.nan
+            fraction_source_bin[m] = numpy.nan
+            percentile_source_bin[m] = numpy.nan
+    
+    with h5py.File(os.path.join(comparison_folder, '{}/SOURCE/SOURCE{}/SELECT.hdf5'.format(tag, index)), 'w') as file:
+        file.create_dataset('select', data=select_source_bin, dtype=bool)
+        
+        file.create_dataset('nmad', data=nmad_source_bin, dtype=numpy.float32)
+        file.create_dataset('fraction', data=fraction_source_bin, dtype=numpy.float32)
+        file.create_dataset('percentile', data=percentile_source_bin, dtype=numpy.float32)
     
     # Duration
     end = time.time()
