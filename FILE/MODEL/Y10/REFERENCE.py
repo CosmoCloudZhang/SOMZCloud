29de0ff1c2b38de48a0996538033e536a03f00b5
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2025-03-07 03:27:34 -0800

    FILE/MODEL/

--- FILE/MODEL/Y10/REFERENCE.py
+++ FILE/MODEL/Y10/REFERENCE.py
@@ -82,7 +82,7 @@ def main(tag, index, folder):
     
     # Reference
     slope = 4.0
-    intercept = 18.0
+    intercept = 18.6
     reference_source = (z1_source <= z_phot) & (z_phot < z2_source)
     reference_lens = (z1_lens <= z_phot) & (z_phot < z2_lens) & (combination_magnitude < slope * z_phot + intercept)
     
