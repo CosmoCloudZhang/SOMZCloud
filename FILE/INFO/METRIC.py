a5f4f014d01e7eafee82d8d0f630135abcd07f61
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2025-04-07 06:44:53 -0700

    FILE/INFO/

---
+++ FILE/INFO/METRIC.py
@@ -0,0 +1,75 @@
+import os
+import yaml
+import h5py
+import json
+import time
+import numpy
+import argparse
+
+
+def main(number, folder):
+    '''
+    Store the fiducial values of density configuration
+    
+    Arguments:
+        number (int): The number of the datasets
+        folder (str): The base folder of the datasets
+    
+    Returns:
+        duration (float): The duration of the process
+    '''
+    # Start
+    start = time.time()
+    
+    # Path
+    info_folder = os.path.join(folder, 'INFO/')
+    dataset_folder = os.path.join(folder, 'DATASET/')
+    
+    # Load
+    with open(os.path.join(dataset_folder, 'CATALOG/OBSERVE.yaml'), 'r') as file:
+        observe = yaml.safe_load(file)
+        observation_list = observe['healpix_pixels']
+        area = observe['sky_area'] / len(observation_list) * len(observation_list) // 2
+    
+    # Definition
+    metric = []
+    sigma0 = 0.26
+    for n in range(number):
+        print('Index: {}'.format(n + 1))
+        
+        metric.append({'Y1': {}, 'Y10': {}})
+        for tag in metric[n].keys():
+            
+            # Application
+            with h5py.File(os.path.join(dataset_folder, '{}/APPLICATION/DATA{}.hdf5'.format(tag, n + 1)), 'r') as file:
+                application_sigma = file['morphology']['sigma'][...]
+            
+            metric[n][tag]['NUMBER'] = len(application_sigma)
+            metric[n][tag]['NUMBER_DENSITY'] = len(application_sigma) / area / 3600
+            metric[n][tag]['EFFECTIVE_NUMBER_DENSITY'] = numpy.sum(numpy.square(sigma0) / (numpy.square(sigma0) + numpy.square(application_sigma))) / area / 3600
+    
+    # Save
+    with open(os.path.join(info_folder, 'METRIC.json'), 'w') as file:
+        json.dump(metric, file, indent=4)
+    
+    # Duration
+    end = time.time()
+    duration = (end - start) / 60
+    
+    # Return
+    print('Time: {:.2f} minutes'.format(duration))
+    return duration
+
+
+if __name__ == '__main__':
+    # Input
+    PARSE = argparse.ArgumentParser(description='Info Metric')
+    PARSE.add_argument('--number', type=int, required=True, help='The number of the datasets')
+    PARSE.add_argument('--folder', type=str, required=True, help='The base folder of the datasets')
+    
+    # Parse
+    NUMBER = PARSE.parse_args().number
+    FOLDER = PARSE.parse_args().folder
+    
+    # Output
+    OUTPUT = main(NUMBER, FOLDER)
---
+++ FILE/INFO/METRIC.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -A m1727
+#SBATCH --nodes=1
+#SBATCH -q regular
+#SBATCH -J INFO_METRIC
+#SBATCH --time=04:00:00
+#SBATCH --mail-type=END
+#SBATCH --constraint=cpu
+#SBATCH -o LOG/%x_%j.out
+#SBATCH --cpus-per-task=256
+#SBATCH --ntasks-per-node=1
+#SBATCH --mail-user=YunHao.Zhang@ed.ac.uk
+
+# Load modules
+module load python
+module load PrgEnv-gnu
+module load cray-mpich/8.1.30
+module load cray-hdf5-parallel
+
+# Activate the conda environment
+source $HOME/.bashrc
+conda activate $CosmoENV
+
+# Initialize the process
+NUMBER=500
+BASE_PATH="/pscratch/sd/y/yhzhang/ZCloud/"
+BASE_FOLDER="/global/cfs/cdirs/lsst/groups/MCP/CosmoCloud/ZCloud/"
+
+# Run applications
+python -u "${BASE_PATH}FILE/INFO/METRIC.py" --number=$NUMBER --folder=$BASE_FOLDER 
