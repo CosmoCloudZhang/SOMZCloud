a775ed3ca701b8e311a18eaf57b09449a05ea6b8
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2025-04-24 13:05:14 -0700

    FILE/FIGURE/

--- FILE/FIGURE/Y1/CONDITIONAL.py
+++ FILE/FIGURE/Y1/CONDITIONAL.py
@@ -73,13 +73,13 @@ def main(tag, index, folder):
     pyplot.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
     pyplot.rcParams['pgf.texsystem'] = 'pdflatex'
     pyplot.rcParams['text.usetex'] = True
-    pyplot.rcParams['font.size'] = 20
+    pyplot.rcParams['font.size'] = 30
     
     # Plot
     bin_size = 5
     range_lens = 0.8
     range_source = 1.2
-    figure, plot = pyplot.subplots(nrows=bin_size, ncols=2, figsize=(12, 3 * bin_size))
+    figure, plot = pyplot.subplots(nrows=bin_size, ncols=2, figsize=(12, 5 * bin_size))
     
     for m in range(bin_size):
         plot[m, 0].plot(z_grid, target_lens[m, :], color='black', linewidth=1.5, linestyle='-')
@@ -97,7 +97,7 @@ def main(tag, index, folder):
         
         plot[m, 0].set_yticks([2, 4, 6, 8])
         plot[m, 0].set_ylabel(r'$\phi \left( z | \mathcal{S}_n, \mathcal{P}_n \right)$')
-        plot[m, 0].text(x=numpy.minimum(numpy.maximum(z1, center_lens[m] - range_lens / 2) + range_lens, z2) - range_lens / 5, y=6.0, s=r'$\mathrm{Bin} \, ' + r'{}$'.format(m + 1), fontsize=20)
+        plot[m, 0].text(x=numpy.minimum(numpy.maximum(z1, center_lens[m] - range_lens / 2) + range_lens, z2) - range_lens / 3, y=6.0, s=r'$\mathrm{Bin} \, ' + r'{}$'.format(m + 1))
         
         if m == 0:
             plot[m, 0].set_title(r'$\mathtt{Lens}$')
@@ -120,7 +120,7 @@ def main(tag, index, folder):
         plot[m, 1].set_xlim(numpy.maximum(z1, center_source[m] - range_source / 2), numpy.minimum(numpy.maximum(z1, center_source[m] - range_source / 2) + range_source, z2))
         
         plot[m, 1].set_yticks([2, 4, 6, 8])
-        plot[m, 1].text(x=numpy.minimum(numpy.maximum(z1, center_source[m] - range_source / 2) + range_source, z2) - range_source / 5, y=6.0, s=r'$\mathrm{Bin} \, ' + r'{}$'.format(m + 1), fontsize=20)
+        plot[m, 1].text(x=numpy.minimum(numpy.maximum(z1, center_source[m] - range_source / 2) + range_source, z2) - range_source / 3, y=6.0, s=r'$\mathrm{Bin} \, ' + r'{}$'.format(m + 1))
         
         if m == 0:
             plot[m, 1].set_title(r'$\mathtt{Source}$')
@@ -131,7 +131,7 @@ def main(tag, index, folder):
     os.makedirs(figure_folder, exist_ok=True)
     os.makedirs(os.path.join(figure_folder, '{}/CONDITIONAL/'.format(tag)), exist_ok=True)
     
-    figure.subplots_adjust(wspace=0.12, hspace=0.24)
+    figure.subplots_adjust(wspace=0.2, hspace=0.2)
     figure.savefig(os.path.join(figure_folder, '{}/CONDITIONAL/FIGURE{}.pdf'.format(tag, index)), format='pdf', bbox_inches='tight')
     
     # Return
--- FILE/FIGURE/Y10/CONDITIONAL.py
+++ FILE/FIGURE/Y10/CONDITIONAL.py
@@ -74,13 +74,13 @@ def main(tag, index, folder):
     pyplot.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
     pyplot.rcParams['pgf.texsystem'] = 'pdflatex'
     pyplot.rcParams['text.usetex'] = True
-    pyplot.rcParams['font.size'] = 20
+    pyplot.rcParams['font.size'] = 30
     
     # Plot
     bin_size = 5
     range_lens = 0.6
     range_source = 1.8
-    figure, plot = pyplot.subplots(nrows=bin_size, ncols=3, figsize=(18, 3 * bin_size))
+    figure, plot = pyplot.subplots(nrows=bin_size, ncols=3, figsize=(18, 5 * bin_size))
     
     for m in range(bin_size):
         plot[m, 0].plot(z_grid, target_lens[m, :], color='black', linewidth=1.5, linestyle='-')
@@ -98,7 +98,7 @@ def main(tag, index, folder):
         
         plot[m, 0].set_yticks([3, 6, 9, 12])
         plot[m, 0].set_ylabel(r'$\phi \left( z | \mathcal{S}_n, \mathcal{P}_n \right)$')
-        plot[m, 0].text(x=numpy.minimum(numpy.maximum(z1, center_lens[m] - range_lens / 2) + range_lens, z2) - range_lens / 5, y=9.0, s=r'$\mathrm{Bin} \, ' + r'{}$'.format(m + 1), fontsize=20)
+        plot[m, 0].text(x=numpy.minimum(numpy.maximum(z1, center_lens[m] - range_lens / 2) + range_lens, z2) - range_lens / 3, y=9.0, s=r'$\mathrm{Bin} \, ' + r'{}$'.format(m + 1))
         
         if m == 0:
             plot[m, 0].set_title(r'$\mathtt{Lens}$')
@@ -121,7 +121,7 @@ def main(tag, index, folder):
         plot[m, 1].set_xlim(numpy.maximum(z1, center_lens[m + bin_size] - range_lens / 2), numpy.minimum(numpy.maximum(z1, center_lens[m + bin_size] - range_lens / 2) + range_lens, z2))
         
         plot[m, 1].set_yticks([3, 6, 9, 12])
-        plot[m, 1].text(x=numpy.minimum(numpy.maximum(z1, center_lens[m + bin_size] - range_lens / 2) + range_lens, z2) - range_lens / 5, y=9.0, s=r'$\mathrm{Bin} \, ' + r'{}$'.format(m + bin_size + 1), fontsize=20)
+        plot[m, 1].text(x=numpy.minimum(numpy.maximum(z1, center_lens[m + bin_size] - range_lens / 2) + range_lens, z2) - range_lens / 3, y=9.0, s=r'$\mathrm{Bin} \, ' + r'{}$'.format(m + bin_size + 1))
         
         if m == 0:
             plot[m, 1].set_title(r'$\mathtt{Lens}$')
@@ -130,13 +130,13 @@ def main(tag, index, folder):
             plot[m, 1].set_xlabel(r'$z$')
     
     for m in range(bin_size):
-        plot[m, 2].plot(z_grid, target_source[m, :], color='darkblue', linewidth=1.5, linestyle='-')
+        plot[m, 2].plot(z_grid, target_source[m, :], color='black', linewidth=1.5, linestyle='-')
         
         plot[m, 2].plot(z_grid, model_source[m, :], color='darkgreen', linewidth=1.5, linestyle='-')
         
         plot[m, 2].plot(z_grid, product_source[m, :], color='darkorange', linewidth=1.5, linestyle='-')
         
-        plot[m, 2].plot(z_grid, histogram_source[m, :], color='black', linewidth=1.5, linestyle='-')
+        plot[m, 2].plot(z_grid, histogram_source[m, :], color='darkblue', linewidth=1.5, linestyle='-')
         
         plot[m, 2].fill_betweenx(y=[0, 6], x1=bin_source[m], x2=bin_source[m + 1], color='gray', alpha=0.5)
         
@@ -144,7 +144,7 @@ def main(tag, index, folder):
         plot[m, 2].set_xlim(numpy.maximum(z1, center_source[m] - range_source / 2), numpy.minimum(numpy.maximum(z1, center_source[m] - range_source / 2) + range_source, z2))
         
         plot[m, 2].set_yticks([2, 4, 6])
-        plot[m, 2].text(x=numpy.minimum(numpy.maximum(z1, center_source[m] - range_source / 2) + range_source, z2) - range_source / 5, y=4.5, s=r'$\mathrm{Bin} \, ' + r'{}$'.format(m + 1), fontsize=20)
+        plot[m, 2].text(x=numpy.minimum(numpy.maximum(z1, center_source[m] - range_source / 2) + range_source, z2) - range_source / 3, y=4.5, s=r'$\mathrm{Bin} \, ' + r'{}$'.format(m + 1))
         
         if m == 0:
             plot[m, 2].set_title(r'$\mathtt{Source}$')
@@ -155,7 +155,7 @@ def main(tag, index, folder):
     os.makedirs(figure_folder, exist_ok=True)
     os.makedirs(os.path.join(figure_folder, '{}/CONDITIONAL/'.format(tag)), exist_ok=True)
     
-    figure.subplots_adjust(wspace=0.12, hspace=0.24)
+    figure.subplots_adjust(wspace=0.2, hspace=0.2)
     figure.savefig(os.path.join(figure_folder, '{}/CONDITIONAL/FIGURE{}.pdf'.format(tag, index)), format='pdf', bbox_inches='tight')
     
     # Return
