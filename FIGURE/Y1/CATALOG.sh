8d82e93d9990773f5e1a6d3c0237e2040d5750fc
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2025-05-15 04:34:52 -0700

    FIGURE/

--- FIGURE/Y1/CATALOG.sh
+++ FIGURE/Y1/CATALOG.sh
@@ -8,7 +8,7 @@
 #SBATCH -o LOG/%x_%j.out
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks-per-node=16
-#SBATCH -J FIGURE_Y1_CONDITIONAL
+#SBATCH -J FIGURE_Y1_CATALOG
 #SBATCH --mail-user=YunHao.Zhang@ed.ac.uk
 
 # Load modules
@@ -30,16 +30,8 @@ export OMP_PLACES=threads
 
 # Initialize the process
 TAG="Y1"
-NUMBER=500
 BASE_PATH="/pscratch/sd/y/yhzhang/SOMZCloud/"
 BASE_FOLDER="/global/cfs/cdirs/lsst/groups/MCP/CosmoCloud/SOMZCloud/"
 
 # Run the application
-for INDEX in $(seq 0 $NUMBER); do
-    srun -N 1 -n 1 -c $SLURM_CPUS_PER_TASK --cpu_bind=cores python -u "${BASE_PATH}FIGURE/${TAG}/CONDITIONAL.py" --tag=$TAG --index=$INDEX --folder=$BASE_FOLDER & 
-    # Control parallel execution
-    if (( $INDEX % $SLURM_NTASKS == 0 )); then
-        wait
-    fi
-done
-wait
+python -u "${BASE_PATH}FIGURE/${TAG}/CATALOG.py" --tag=$TAG --folder=$BASE_FOLDER
