f8c47a86d811899f2730fce39b4b15f14832e66b
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2025-08-13 14:34:43 -0700

    CONSTRAIN/

---
+++ CONSTRAIN/Y1/ESTIMATE.py
@@ -0,0 +1,90 @@
+import os
+import time
+import yaml
+import argparse
+
+
+def main(tag, index, folder):
+    '''
+    Main function to create the constrain estimator configuration file.
+    
+    Arguments:
+        tag (str): The tag of the configuration
+        index (int): The index of all the datasets
+        folder (str): The base folder of all the datasets
+    
+    Returns:
+        duration (float): The duration of the function in minutes
+    '''
+    # Start
+    start = time.time()
+    print('Index: {}'.format(index))
+    
+    # Path
+    constrain_folder = os.path.join(folder, 'CONSTRAIN/')
+    
+    # Config
+    config = {
+        'ESTIMATE{}'.format(index): {
+            'aliases': {
+                'name': 'input_name',
+                'input': 'input_data', 
+                'model': 'input_model',
+                'output': 'output_data'
+            }, 
+            'chunk_size': 100000, 
+            'nondetect_val': 99.0, 
+            'ref_band': 'mag_i_lsst', 
+            'output_mode': 'default',
+            'qp_representation': 'interp',
+            'hdf5_groupname': 'photometry', 
+            'zmin': 0.0, 'zmax': 3.0, 'nzbins': 301, 
+            'bands': [
+                'mag_u_lsst', 
+                'mag_g_lsst', 
+                'mag_r_lsst', 
+                'mag_i_lsst', 
+                'mag_z_lsst', 
+                'mag_y_lsst'
+            ], 
+            'err_bands': [
+                'mag_u_lsst_err', 
+                'mag_g_lsst_err', 
+                'mag_r_lsst_err', 
+                'mag_i_lsst_err', 
+                'mag_z_lsst_err', 
+                'mag_y_lsst_err'
+            ]
+        }
+    }
+    
+    os.makedirs(os.path.join(constrain_folder, '{}/'.format(tag)), exist_ok=True)
+    os.makedirs(os.path.join(constrain_folder, '{}/ESTIMATE/'.format(tag)), exist_ok=True)
+    
+    config_name = os.path.join(constrain_folder, '{}/ESTIMATE/ESTIMATE{}.yaml'.format(tag, index))
+    with open(config_name, 'w') as config_file:
+        yaml.dump(config, config_file, default_flow_style=False)
+    
+    # End
+    end = time.time()
+    duration = (end - start) / 60
+    
+    # Return
+    print('Time: {:.2f} minutes'.format(duration))
+    return duration
+
+
+if __name__ == '__main__':
+    # Input
+    PARSE = argparse.ArgumentParser(description='Constrain Estimator')
+    PARSE.add_argument('--tag', type=str, required=True, help='The tag of the configuration')
+    PARSE.add_argument('--index', type=int, required=True, help='The index of all the datasets')
+    PARSE.add_argument('--folder', type=str, required=True, help='The base folder of all the datasets')
+    
+    # Parse
+    TAG = PARSE.parse_args().tag
+    INDEX = PARSE.parse_args().index
+    FOLDER = PARSE.parse_args().folder
+    
+    # Output
+    OUTPUT = main(TAG, INDEX, FOLDER)
---
+++ CONSTRAIN/Y1/ESTIMATE.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#SBATCH -A m1727
+#SBATCH --nodes=4
+#SBATCH -q regular
+#SBATCH --time=04:00:00
+#SBATCH --mail-type=END
+#SBATCH --constraint=cpu
+#SBATCH -o LOG/%x_%j.out
+#SBATCH --cpus-per-task=2
+#SBATCH --ntasks-per-node=128
+#SBATCH -J CONSTRAIN_Y1_ESTIMATE
+#SBATCH --mail-user=YunHao.Zhang@ed.ac.uk
+
+# Load modules
+module load python
+module load PrgEnv-gnu
+module load cray-mpich/8.1.30
+module load cray-hdf5-parallel
+
+# Activate the conda environment
+source $HOME/.bashrc
+conda activate $RAILENV
+
+# Set environment
+export NUMEXPR_MAX_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export HDF5_USE_FILE_LOCKING=FALSE
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+
+# Initialize the process
+TAG="Y1"
+NUMBER=500
+BASE_PATH="/pscratch/sd/y/yhzhang/SOMZCloud/"
+BASE_FOLDER="/global/cfs/cdirs/lsst/groups/MCP/CosmoCloud/SOMZCloud/"
+
+for INDEX in $(seq 0 $NUMBER); do
+    # Set variables
+    NAME="ESTIMATE${INDEX}"
+    MODEL_PATH="${BASE_FOLDER}CONSTRAIN/${TAG}/INFORM/INFORM${INDEX}.pkl"
+    INPUT_PATH="${BASE_FOLDER}DATASET/${TAG}/APPLICATION/DATA${INDEX}.hdf5"
+    CONFIG_PATH="${BASE_FOLDER}CONSTRAIN/${TAG}/ESTIMATE/ESTIMATE${INDEX}.yaml"
+    OUTPUT_PATH="${BASE_FOLDER}CONSTRAIN/${TAG}/ESTIMATE/ESTIMATE${INDEX}.hdf5"
+    # Run applications
+    python -u "${BASE_PATH}CONSTRAIN/${TAG}/ESTIMATE.py" --tag=$TAG --index=$INDEX --folder=$BASE_FOLDER &&
+    srun -u -N 1 -n 1 -c $SLURM_CPUS_PER_TASK python -m ceci rail.estimation.algos.flexzboost.FlexZBoostEstimator --mpi --name=$NAME --input=$INPUT_PATH --model=$MODEL_PATH --config=$CONFIG_PATH --output=$OUTPUT_PATH &
+    # Control parallel execution
+    if (( $INDEX % $SLURM_NTASKS == 0 )); then
+        wait
+    fi
+done
+wait
