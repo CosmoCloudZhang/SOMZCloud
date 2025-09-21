652c5331a372c2c0c72ade2642517b56f3b5c3d5
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2025-09-21 14:48:19 -0700

    CELL/

--- CELL/Y10/SS/SHIFT.sh
+++ CELL/Y10/SS/SHIFT.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH -A m1727
-#SBATCH --nodes=1
+#SBATCH --nodes=4
 #SBATCH -q regular
-#SBATCH --time=48:00:00
+#SBATCH --time=04:00:00
 #SBATCH --mail-type=END
 #SBATCH --constraint=cpu
 #SBATCH -o LOG/%x_%j.out
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=32
+#SBATCH --ntasks-per-node=8
 #SBATCH -J CELL_Y10_SS_SHIFT
-#SBATCH --ntasks-per-node=32
 #SBATCH --mail-user=YunHao.Zhang@ed.ac.uk
 
 # Load modules
@@ -30,17 +30,16 @@ export OMP_PLACES=threads
 
 # Initialize the process
 TAG="Y10"
-NAME="SS"
 BASE_PATH="/pscratch/sd/y/yhzhang/SOMZCloud/"
 BASE_FOLDER="/global/cfs/cdirs/lsst/groups/MCP/CosmoCloud/SOMZCloud/"
 
 # Run applications
-LABEL_LIST=("ZERO" "HALF" "UNITY" "DOUBLE")
-RANK_LIST=("DIR" "FIDUCIAL" "STACK" "PRODUCT" "TRUTH")
+LABEL_LIST=("DIR"  "STACK" "HYBRID" "TRUTH")
+NAME_LIST=("COPPER" "GOLD" "IRON" "SILVER" "TITANIUM" "ZINC")
 
-for LABEL in "${LABEL_LIST[@]}"; do
-    for RANK in "${RANK_LIST[@]}"; do
-        srun -u -N 1 -n 1 -c $SLURM_CPUS_PER_TASK python -u "${BASE_PATH}CELL/${TAG}/${NAME}/SHIFT.py" --tag=$TAG --name=$NAME --rank=$RANK --label=$LABEL --folder=$BASE_FOLDER &
+for NAME in "${NAME_LIST[@]}"; do
+    for LABEL in "${LABEL_LIST[@]}"; do
+        srun -u -N 1 -n 1 -c $SLURM_CPUS_PER_TASK python -u "${BASE_PATH}CELL/${TAG}/SS/SHIFT.py" --tag=$TAG --name=$NAME --label=$LABEL --folder=$BASE_FOLDER & 
     done
 done
 wait
