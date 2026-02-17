3be6036554efeae7f7236387bfc30d646dfbcb54
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2026-02-17 00:05:55 -0800

    CELL/

--- CELL/Y1/COVARIANCE/GAUSSIAN.sh
+++ CELL/Y1/COVARIANCE/GAUSSIAN.sh
@@ -36,7 +36,7 @@ BASE_FOLDER="/global/cfs/cdirs/lsst/groups/MCP/CosmoCloud/SOMZCloud/"
 # Run applications
 NAME_LIST=("COPPER" "GOLD" "IRON" "SILVER" "TITANIUM" "ZINC")
 for NAME in "${NAME_LIST[@]}"; do
-    python -u "${BASE_PATH}CELL/${TAG}/COVARIANCE/GAUSSIAN.py" --tag=$TAG --name=$NAME --folder=$BASE_FOLDER &
+    python -u "${BASE_PATH}CELL/${TAG}/COVARIANCE/GAUSSIAN.py" --tag=$TAG --name=$NAME --folder=$BASE_FOLDER &&
     srun -u -N 1 -n 1 -c $SLURM_CPUS_PER_TASK python /global/homes/y/yhzhang/opt/OneCovariance/covariance.py "${BASE_FOLDER}/CELL/${TAG}/COVARIANCE/${NAME}/CONFIG.ini" &
 done
 wait
