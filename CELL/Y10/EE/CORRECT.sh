847b44ec0be8b5dc228e523276763e18750faa26
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2026-02-22 14:57:21 -0800

    CELL/

--- CELL/Y10/EE/CORRECT.sh
+++ CELL/Y10/EE/CORRECT.sh
@@ -2,7 +2,7 @@
 #SBATCH -A m1727
 #SBATCH --nodes=1
 #SBATCH -q regular
-#SBATCH --time=36:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mail-type=END
 #SBATCH --constraint=cpu
 #SBATCH -o LOG/%x_%j.out
